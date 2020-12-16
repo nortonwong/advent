5fd8c77168ed4d1c6b550707c13c16556115f953
Norton Wong <nortonwong@users.noreply.github.com>
2020-12-16 01:51:22 -0500

    Upload additional solutions.

---
+++ 2015/19.js
@@ -0,0 +1,47 @@
+/* 2015-19 Molecule */
+((input = document.body.innerText.trim().split('\n')) => {
+	const goal = input.pop();
+	const [forward, backward] = input.filter(s => s).reduce(([forward, backward], rule) => {
+		const [from, to] = rule.split(' => ');
+		(forward[from] ??= []).push(to);
+		(backward[to] ??= []).push(from);
+		return [forward, backward];
+	}, [{}, {}]);
+	function go(state, direction) {
+		const targets = Object.keys(direction);
+		const result = new Set();
+		for (const possibility of state) {
+			for (let i = 0; i < possibility.length; i++) {
+				if (possibility[i] === possibility[i].toLowerCase()) {
+					continue;
+				}
+				const prev = possibility.substring(0, i);
+				const rest = possibility.substring(i);
+				const hits = targets.filter(t => rest.startsWith(t));
+				for (const hit of hits) {
+					for (const mark of direction[hit]) {
+						const aftermath = prev + mark + rest.substring(hit.length);
+						result.add(aftermath);
+					}
+				}
+			}
+		}
+		return result;
+	}
+	const answerA = go(new Set([goal]), forward).size;
+	const answerB = async () => {
+		let state = [[goal, 0]];
+		while (state.length) {
+			await new Promise(resolve => setTimeout(resolve), 1);
+			const [next, turn] = state.pop();
+			console.log('Depth-first search with queue of', state.length, '... Turn', turn);
+			const result = go([next], backward);
+			if (result.has('e')) {
+				return turn + 1;
+			}
+			const history = [...result].map(e => [e, turn + 1]);
+			state.push(...history);
+		}
+	};
+	answerB().then(answerB => console.log([answerA, answerB]));
+})();
---
+++ 2020/16.js
@@ -0,0 +1,60 @@
+/* 2020-16 Train */
+(async (input = document.body.innerText.trim().split('\n\n')) => {
+	const [fields, [ , self], [ , ...others]] = input.map(i => i.split('\n'));
+	const predicates = fields.map(field => {
+		const [name, desc] = field.split(': ');
+		const ranges = desc.split(' or ').map(range => {
+			const [min, max] = range.split('-').map(Number);
+			return d => d >= min && d <= max;
+		});
+		const test = d => ranges.some(test => test(d));
+		test.fieldName = name;
+		return test;
+	});
+	function obviousErrors(ticket) {
+		ticket = ticket.split(',').map(Number);
+		return ticket.filter(t => !predicates.some(p => p(t)));
+	}
+	const errorScore = others.flatMap(obviousErrors).reduce((a, b) => a + b, 0);
+	const tickets = [self, ...others.filter(t => !obviousErrors(t).length)]
+		.map(t => t.split(',').map(Number));
+	function correlates(fi, tfi) {
+		const test = predicates[fi];
+		return tickets.every(t => test(t[tfi]));
+	}
+	const N = fields.length;
+	const range = Array.from({ length: N }).map((_, i) => i);
+	const unsolved = new Set(range); // Set<FI>
+	let solved = []; // Map<FI, TFI>
+	while (unsolved.size) {
+		console.log('Solving', ...unsolved, '...');
+		async function narrow() {
+			for (const fi of unsolved) {
+				const tfis = [];
+				for (const tfi of range) {
+					if (solved.includes(tfi)) {
+						continue;
+					}
+					await new Promise(resolve => setTimeout(resolve), 1);
+					if (correlates(fi, tfi)) {
+						tfis.push(tfi);
+					}
+				}
+				if (tfis.length === 1) {
+					unsolved.delete(fi);
+					solved[fi] = tfis[0];
+					return true;
+				}
+			}
+		}
+		if (!(await narrow())) {
+			throw unsolved;
+		}
+	}
+	const departureScore = solved
+		.map((tfi, fi) => [predicates[fi], tfi])
+		.filter(([p, tfi]) => p.fieldName.startsWith('departure '))
+		.map(([ , tfi]) => tickets[0][tfi])
+		.reduce((a, b) => a * b);
+	return [errorScore, departureScore]
+})().then(console.log);
